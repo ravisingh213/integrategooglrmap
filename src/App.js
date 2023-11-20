536e059921c21125deef56dcd4accb62901ef5b7
ravisingh213 <ravibhai213@gmail.com>
2023-11-20 21:22:01 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>ggggg</code>
         </p>
         <a
           className="App-link"
